2a796ae3fafa045db611e3c95f67e544f0175e9d
swalters6511 <stuartjwalters@gmail.com>
2023-02-01 03:32:55 -0500

    fixed typo in script

--- install.sh
+++ install.sh
@@ -8,6 +8,5 @@ mkdir -p "$HOME/.config/nvim/"
 mkdir -p "$HOME/.config/nvim/undo"
 ln -sf "$HOME/dotfiles/nvim/init.lua" "$HOME/.config/nvim"
 ln -s "$HOME/dotfiles/X11/" "$HOME/.config/"
-rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 
