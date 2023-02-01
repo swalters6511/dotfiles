4290969570568a4f9e44e1f6460e74ca2c47ae46
swalters6511 <stuartjwalters@gmail.com>
2023-02-01 03:23:56 -0500

    changed install.sh to include and not delete X11/.Xresources

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@
 mkdir -p "$HOME/.config/nvim/"
 mkdir -p "$HOME/.config/nvim/undo"
 ln -sf "$HOME/dotfiles/nvim/init.lua" "$HOME/.config/nvim"
-rm -rf "$HOME/.config/X11/"
 ln -s "$HOME/dotfiles/X11/" "$HOME/.config/"
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
